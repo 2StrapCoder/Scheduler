ec266b4db2ebfed79faf9fe079da2a09ab963bd5
christopher miguel <chrismiguel115@gmail.com>
2023-10-09 21:54:24 -0500

    a save for time stamps for this scheduler

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,9 @@ $(function () {
     var currentHour = dayjs().hour();
     $('#currentDay').text(dayjs().format('MMM d, YYYY'));
 
-  
+    $('.saveBtn').on('click', function(){
+        var timeBlock = $(this).closest('.time-block');
+        var hourId = timeBlock.attr('id');
+        var userInput = timeBlock.find('textarea').val();
+        localStorage.setItem(hourId, userInput);
+    });
